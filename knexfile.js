2512a9ceb4d64c90d5e8c2b15437487b78448a00
Keven Andrade <hirlandeer7@gmail.com>
2021-05-21 14:34:20 -0100

    link knext to mysql DB

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: "cumpraliv0",
+      user: "root",
+      password: ""
+    }
+  },
+};
