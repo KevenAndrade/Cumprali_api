68fc3716f24db70cde25bc3ccae3ddbe3811ead2
Keven Andrade <hirlandeer7@gmail.com>
2021-05-25 11:22:12 -0100

    paginacao

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -2,12 +2,19 @@ const knex = require('../database');
 
 module.exports ={
     async lista(req, res) {
-        const results = await knex('tblproduto')
+
+        const { page = 1 } = req.query;
+        const results = await knex('tblproduto').limit(5).offset((page - 1) * 5)
+
+        const [ count ] = await knex('tblproduto').count();
+        console.log( count );
+        
+        res.header('X-total-count',count["count(*)"] )
 
         return res.json(results)
     },
 
-    async listaOne(req, res) {
+    async listaOne(req, res,next) {
         try {
             const { id } = req.params;
             const query =  knex('tblproduto')
@@ -15,10 +22,12 @@ module.exports ={
             if (id) {
                 query
                     .where({ idProduto: id })
-                    .join('tblsubcategoria', 'tblsubcategoria.IdSubCategoria', '=', 'tblproduto.IdProduto')
-                    .select('tblproduto.*', {subcategoria: 'tblsubcategoria.Nome'})
+                    .join('tblsubcategoria', 'tblsubcategoria.IdSubCategoria', '=', 'tblproduto.IdSubCategoria')
+                    .join('tblvendedor', 'tblvendedor.IdVendedor', '=', 'tblproduto.IdVendedor')
+                    .select('tblproduto.*', {subcategoria: 'tblsubcategoria.Nome'}, {nomeVendedor: 'tblvendedor.Nome'})
             }
 
+            
             results = await query
             return res.json(results)
 
